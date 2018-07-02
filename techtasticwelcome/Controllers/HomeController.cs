4f4a06f63442bc38e1d88036c3df2b4882e9e0a2
Graham Small <graham.small@eon.com>
2018-07-02 11:21:51 +0100

    Added greeting message to model

--- techtasticwelcome/Controllers/HomeController.cs
+++ techtasticwelcome/Controllers/HomeController.cs
@@ -4,15 +4,24 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using techtasticwelcome.Models;
 
 namespace techtasticwelcome.Controllers
 {
     public class HomeController : Controller
     {
+        private IConfiguration _configuration { get; }
+
+        public HomeController (IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var model = _configuration["Greeting"];
+            return View("Index", model);
         }
 
         public IActionResult About()
