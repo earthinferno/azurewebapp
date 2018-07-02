9ade34f52ee3360008fd39cb246ec9d3fc806c93
Graham Small <graham.small@eon.com>
2018-07-02 10:14:33 +0100

    Add project files.

---
+++ techtasticwelcome/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace techtasticwelcome.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
