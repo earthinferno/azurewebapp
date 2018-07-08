3b91dc359347984f5f976e839f7024d5a992217a
Graham Small <graham.small@eon.com>
2018-07-08 11:02:34 +0100

    Updating to work with Azure sql

--- techtasticwelcome/Data/Migrations/20180708093431_initial.Designer.cs
+++ techtasticwelcome/Data/Migrations/20180708093431_initial.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using techtasticwelcome.Data;
 
 namespace techtasticwelcome.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180708093431_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- techtasticwelcome/Program.cs
+++ techtasticwelcome/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using techtasticwelcome.Data;
 
 namespace techtasticwelcome
 {
@@ -14,9 +17,21 @@ namespace techtasticwelcome
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+            MigrateDatabase(host);
+            host.Run();
+
         }
 
+        public static void MigrateDatabase(IWebHost host)
+        {
+            //var scope = host.Services.GetService(ApplicationDbContext);
+            using (var scope = host.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                context.Database.Migrate();
+            }
+        }
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseApplicationInsights()
