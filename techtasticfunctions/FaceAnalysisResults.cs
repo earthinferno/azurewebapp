3ab557c93e006cc9fb73d50ab5fd818964d74158
Graham Small <graham.small@eon.com>
2018-08-16 16:16:03 +0100

    add image analysis results storage

---
+++ techtasticfunctions/FaceAnalysisResults.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ProjectOxford.Face.Contract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace techtasticfunctions
+{
+    public class FaceAnalysisResults
+    {
+        public Face[] Faces { get; set; }
+        public string ImageId { get; set; }
+    }
+}
--- techtasticfunctions/ImageAnalysis.cs
+++ techtasticfunctions/ImageAnalysis.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.ProjectOxford.Face;
@@ -11,7 +12,10 @@ namespace techtasticfunctions
     public static class ImageAnalysis
     {
         [FunctionName("ImageAnalysis")]
-        public static void Run([BlobTrigger("images/{name}", Connection = "techtasticstorage")]CloudBlockBlob myBlob, string name, TraceWriter log)
+        public static async Task Run([BlobTrigger("images/{name}", Connection = "techtasticstorage")]
+        CloudBlockBlob myBlob, string name, TraceWriter log,
+        [CosmosDB("welcometotechtastic", "ttimagesanalysis", ConnectionStringSetting = "ttdb")]
+        IAsyncCollector<FaceAnalysisResults> result )
         {
             log.Info($"C# Blob trigger function Processed blob\n Name:{myBlob.Name} \n Size: {myBlob.Properties.Length} Bytes");
 
@@ -19,12 +23,18 @@ namespace techtasticfunctions
             var url = myBlob.Uri + sas;
 
             log.Info($"Blob URL is {url}");
+
+            var faces = await GetAnalysisAsync(url);
+            await result.AddAsync(new FaceAnalysisResults { Faces = faces, ImageId = myBlob.Name });
         }
 
-        //public static async Task<Face[]> GetAnalysisAsync(string url)
-        //{
-        //    var clinet = new FaceServiceClient("1672b8d7de0a41ddab3d9139a956dc76", "https://westeurope.api.cognitive.microsoft.com/face/v1.0");
-        //}
+        public static async Task<Face[]> GetAnalysisAsync(string url)
+        {
+            var client = new FaceServiceClient("1672b8d7de0a41ddab3d9139a956dc76", "https://westeurope.api.cognitive.microsoft.com/face/v1.0");
+            var types = new[] { FaceAttributeType.Emotion };
+            var result = await client.DetectAsync(url, false, false, types);
+            return result;
+        }
 
         private static string GetSas(CloudBlockBlob blob)
         {
